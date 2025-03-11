f18df27b3f8b5b625730d314aef562cb01d6aa0c
Bppatkar <bhanupratappatkar777@gmail.com>
2025-03-11 09:09:24 +0530

    fetching api with some filter functionality

---
+++ src/App.jsx
@@ -0,0 +1,67 @@
+import React, { useEffect, useState } from "react";
+
+import ProductCard from "./ProductCard";
+
+function App() {
+  const [products, setproducts] = useState([]);
+  const [searchProduct, setSearchProduct] = useState("");
+  const [filteredProducts, setfilteredProducts] = useState([]);
+
+  useEffect(() => {
+    fetchingData();
+  }, []);
+
+  const fetchingData = async () => {
+    try {
+      const response = await fetch("https://fakestoreapi.com/products");
+      const data = await response.json();
+      setproducts(data);
+      setfilteredProducts(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
+  return (
+    <>
+      <div className="input-field">
+        <input
+          type="text"
+          placeholder="Search"
+          onChange={(e) => setSearchProduct(e.target.value)}
+          value={searchProduct}
+        />
+        <button
+          onClick={() =>
+            setfilteredProducts(
+              products.filter((product) =>
+                product.title
+                  .toLowerCase()
+                  .includes(searchProduct.toLowerCase())
+              )
+            )
+          }
+        >
+          Search
+        </button>
+      </div>
+      <div className="products">
+        {filteredProducts.length > 0 ? (
+          filteredProducts.map((product) => (
+            <ProductCard
+              key={product.id}
+              image={product.image}
+              title={product.title}
+              price={product.price}
+              rating={product.rating?.rate}
+            />
+          ))
+        ) : (
+          <p>Loading...</p>
+        )}
+      </div>
+    </>
+  );
+}
+
+export default App;
